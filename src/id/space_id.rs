e746010ee430149fde1416eebcf925b7a5010856
Tomoro Saito <hg1190159@hiroogakuen.com>
2025-12-11 00:25:48 +0900

    SpaceID TraitのSectionを追加

--- src/id/space_id.rs
+++ src/id/space_id.rs
@@ -12,6 +12,24 @@ pub mod range;
 pub mod segment;
 pub mod single;
 
+///本ライブラリが扱う空間IDは2種類存在します。二つの空間ID型はIPAの定めた標準的な空間ID形状を満たし、また相互に変換が可能です。
+/// 求められるパフォーマンスに応じて二つのIDを使い分けることを推奨します。また、混在しても基本的に問題ない設計になっています。
+///
+/// SpaceID Traitは全ての空間ID型が満たすべき基本的な共通性質を定義しています。
+///
+/// # [SingleID]と[RangeID]の違い
+///
+/// # 移動の`bound`と`warp`について
+///
+/// # 使い分けのガイドライン
+///
+/// # 混在が可能かどうか
+/// /// <details>
+/// <summary>クリックして展開（日本語）</summary>
+///
+/// ここに詳細な説明を書く。
+///
+/// </details>
 pub trait SpaceID {
     //そのIDの各次元の最大と最小を返す
     fn min_f(&self) -> i64;
@@ -33,4 +51,8 @@ pub trait SpaceID {
     fn wrap_south(&mut self, by: u64);
     fn wrap_east(&mut self, by: u64);
     fn wrap_west(&mut self, by: u64);
+
+    //中心を求める関数
+
+    //頂点を求める関数
 }
