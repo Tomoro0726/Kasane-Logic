424d03b6de4a4eb05e8a49dd747366e6a8ce45ec
Tomoro Saito <hg1190159@hiroogakuen.com>
2025-12-15 16:22:46 +0900

    reset main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,1 @@
-use kasane_logic::{
-    geometry::coordinate::Coordinate,
-    id::space_id::{SpaceID, range::RangeID},
-};
-
-fn main() {
-    let id = RangeID::new(5, [-10, -5], [8, 9], [5, 10]).unwrap();
-
-    let vertices: [Coordinate; 8] = id.vertices();
-    println!("{:?}", vertices);
-}
+fn main() {}
